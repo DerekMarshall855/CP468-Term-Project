10d212b6e9ae35145dce5b5be9d8c7a9a41390bb
Joanne <buix9830@mylaurier.ca>
2020-12-10 17:23:39 -0500

    added a timer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # IMPORTS
 from genetic_algorithm import *
 import matplotlib.pyplot as plt
+import time
 
 # Max number of breed calls until giving up/settling (Settle at local max/min)
 GENS = 300
@@ -12,8 +13,8 @@ retain = 0.15
 randRetain = 0.03
 lowBound = -1
 highBound = 1
-benchmark = 4  # 1 - DeJongs 2 - Rosenbrocks 3 - Himmelblaus 4 - size25(memSize >=51) 5 - size27(memSize >= 55)
-memSize = 51  # Usually 20 or lower for benchmark 1-3, 4 - 51, 5 - 55, 6 - 59, 7 - 63, 8 - 67
+benchmark = 1  # 1 - DeJongs 2 - Rosenbrocks 3 - Himmelblaus 4 - size25(memSize >=51) 5 - size27(memSize >= 55)
+memSize = 10  # Usually 20 or lower for benchmark 1-3, 4 - 51, 5 - 55, 6 - 59, 7 - 63, 8 - 67
 binary = True  # If True low and high bound are binary (Each value is one or the other), else not binary
 
 pop = Population(size=populationSize,
@@ -26,12 +27,16 @@ pop = Population(size=populationSize,
                  memSize=memSize,
                  binary=binary)
 
+start = time.time()
+
 for i in range(GENS):
     pop.avg_fitness(gen=i)
     pop.next_gen()
 
     if pop.onTarget:
         print("Finished at gen: ", i, ", Avg Population Fitness: ", pop.history[-1])
+        runtime = time.time() - start
+        print("runtime: %.3f minutes" % (runtime/60))
         break
 
 if not pop.onTarget:
